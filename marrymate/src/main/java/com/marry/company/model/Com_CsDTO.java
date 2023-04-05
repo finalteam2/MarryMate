1ae95672f84ed3166f1a91195052e5999638f734
김진혁 <kjh153426@gmail.com>
2023-04-05 11:59:22 +0900

    com_csDTO 변경

--- marrymate/src/main/java/com/marry/company/model/Com_CsDTO.java
+++ marrymate/src/main/java/com/marry/company/model/Com_CsDTO.java
@@ -6,65 +6,121 @@ public class Com_CsDTO {
 	private int ccsidx;
 	private int cidx;
 	private int midx;
-	private int type;
 	private String subject;
 	private String content;
 	private Date csdate;
+	private String answer;
+	private Date ansdate;
+	private int is_private;
+	
+	
 	public Com_CsDTO() {
 		super();
 	}
-	public Com_CsDTO(int ccsidx, int cidx, int midx, int type, String subject, String content, Date csdate) {
+
+
+	public Com_CsDTO(int ccsidx, int cidx, int midx, String subject, String content, Date csdate, String answer,
+			Date ansdate, int is_private) {
 		super();
 		this.ccsidx = ccsidx;
 		this.cidx = cidx;
 		this.midx = midx;
-		this.type = type;
 		this.subject = subject;
 		this.content = content;
 		this.csdate = csdate;
+		this.answer = answer;
+		this.ansdate = ansdate;
+		this.is_private = is_private;
 	}
+
+
 	public int getCcsidx() {
 		return ccsidx;
 	}
+
+
 	public void setCcsidx(int ccsidx) {
 		this.ccsidx = ccsidx;
 	}
+
+
 	public int getCidx() {
 		return cidx;
 	}
+
+
 	public void setCidx(int cidx) {
 		this.cidx = cidx;
 	}
+
+
 	public int getMidx() {
 		return midx;
 	}
+
+
 	public void setMidx(int midx) {
 		this.midx = midx;
 	}
-	public int getType() {
-		return type;
-	}
-	public void setType(int type) {
-		this.type = type;
-	}
+
+
 	public String getSubject() {
 		return subject;
 	}
+
+
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
+
+
 	public String getContent() {
 		return content;
 	}
+
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
+
 	public Date getCsdate() {
 		return csdate;
 	}
+
+
 	public void setCsdate(Date csdate) {
 		this.csdate = csdate;
 	}
-	
+
+
+	public String getAnswer() {
+		return answer;
+	}
+
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+
+	public Date getAnsdate() {
+		return ansdate;
+	}
+
+
+	public void setAnsdate(Date ansdate) {
+		this.ansdate = ansdate;
+	}
+
+
+	public int getIs_private() {
+		return is_private;
+	}
+
+
+	public void setIs_private(int is_private) {
+		this.is_private = is_private;
+	}
 	
 }
